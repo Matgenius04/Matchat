f9fdd504dbcf7812ccaad0f7784b88e5d2c03209
Jonathan Chen <jonathan.chen@wustl.edu>
2017-11-23 21:33:17 -0500

    Monneyy

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,23 @@
+const messageHTML = ({ sender, msg, color }) => {
+  console.log(`${sender}: ${msg}`)
+  return $('<li>')
+    .append($(
+      `<div class="message">
+        <span class="user" style="color: ${color}">${sender}</span>&nbsp;-&nbsp;<span>${msg}</span>
+      </div>
+    `))
+}
+
+const getRandomColor = () => {
+  const rand = (min, max) => min + Math.random() * (max - min)
+
+  let h = rand(1, 360)
+  let s = 100
+  let l = rand(3, 6) * 10
+
+  return `hsl(${h},${s}%,${l}%)`
+}
+
 $(function () {
   let socket = io()
   let nextMessageTime = Date.now()
@@ -19,15 +39,30 @@ $(function () {
 
     return false;
   })
-  socket.on('chat message', msg => {
-    $('#messages').append($('<li>').text(msg))
-  })
-})
 
-$(document).ready(() => {
-  $('#username').keypress(ev => {
-    if (ev.which == 13) {
-      $("#popup").hide()
-    }
+  socket.on('chat message', message => {
+    $('#messages').append(messageHTML(message))
   })
+
+  $(document).ready(() => {
+    $('#username').keypress(ev => {
+      if (ev.which == 13) {
+
+        let user = $('#username').val()
+        let color = getRandomColor()
+
+        socket.emit('user', {
+          user,
+          color
+        })
+
+        $("#popup").hide()
+      }
+    })
+
+    $('#add-room').click(ev => {
+      console.log('add')
+    })
+  })
+
 })
--- src/io/index.js
+++ src/io/index.js
@@ -1,11 +1,31 @@
 
 module.exports = http => {
   const io = require('socket.io')(http)
+  const users = {}
 
   io.on('connection', socket => {
-    socket.on('chat message', msg => {
-      io.emit('chat message', msg)
+
+    socket.on('user', data => {
+      users[socket.id] = {
+        name: data.user,
+        color: data.color
+      }
     })
+
+    socket.on('chat message', msg => {
+      const user = users[socket.id]
+
+      io.emit('chat message', {
+        sender: user.name,
+        color: user.color,
+        msg
+      })
+    })
+
+    socket.on('disconnect', () => {
+      delete users[socket.id]
+    })
+
   })
 
   return io
